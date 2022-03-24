ac7c504f30c8969bf7a1f7889db2d1ddc1d89199
Moshe <moshikot015@gmail.com>
2022-03-24 18:29:37 +0200

    .

---
+++ VMwareReport_VC_VIEW.ps1
@@ -0,0 +1,71 @@
+﻿##Params_Below
+$DomainName = "mosh.corp"
+$OU = 'OU=קבוצה נוספת,OU=עוד קבוצה,OU=טכנולוגיה,OU=מוש,OU=moshe,DC=mosh,DC=corp'
+#End Hardcodded params
+
+
+#Connect to View, Vsphere servers.
+try {
+	$HVServer = Connect-HVServer -Server HVSERVERHERE.mosh.corp -Domain $DomainName
+	$VIServer = Connect-VIServer -Server VISERVERHERE.mosh.corp
+}
+
+catch {
+	Write-Host -ForegroundColor Red "Error connecting to VMware Horizon"
+	Write-Host "If you don't have VMware powershell tools installed, Please install it via:"
+	Write-Host "PATH TO INSTALL HERE"
+	break
+}
+
+
+#Get all users in OU to Array
+[array]$OU_users = (Get-Aduser -Filter * -SearchBase $OU).SamAccountName
+
+#Find all machines with OU users assigments and put them into array
+$OU_UsersMachines = $null
+foreach ($OU_User in $OU_users) {
+	[array]$OU_UsersMachines += Get-HVMachineSummary | Where-Object { $_.NamesData.Username -contains "$($DomainName)\$($OU_user)" }
+
+}
+##Processing machine information to array - Export to CSV
+$vms = $OU_UsersMachines
+$results = @()
+foreach ($vm in $vms) {
+	Write-Host "Collecting info for $($vm.Base.Name)"
+	$properties = @{
+		MachineName = $vm.Base.Name
+		DNSNAME = $vm.Base.DNSNAME
+		VDIPool = $vm.NamesData.DesktopName
+		Username = $vm.NamesData.Username
+		Num_CPUs = Get-VM -Name $vm.Base.Name | Select-Object -ExpandProperty NumCPU
+		RAM = Get-VM -Name $vm.Base.Name | Select-Object -ExpandProperty MemoryMB	}
+	$results += New-Object psobject -Property $properties
+}
+
+$results | Export-Csv -NoTypeInformation -Path C:\temp\PersistentInfo.csv
+
+
+
+
+#Second part of mission 1
+
+$ViewPools = $null
+#Get all pools with Linked Clone , Instant clone types and processing data. - Export to CSV
+$ViewPools = Get-hvpool | Where-Object { $_.Source -eq "VIEW_COMPOSER" -or $_.Source -eq "INSTANT_CLONE_ENGINE" }
+foreach ($ViewPool in $ViewPools) {
+	$SourceType = $null
+	switch ($ViewPool.Source) {
+		'VIEW_COMPOSER' { $SourceType = "Linked_Clone" }
+		'INSTANT_CLONE_ENGINE' { $SourceType = "Instant_Clone" }
+		default { $SourceType = "Unknown" }
+	}
+	$row = New-Object System.Object
+	$row | Add-Member -MemberType NoteProperty -Name "PoolName" -Value $ViewPool.Base.Name
+	$row | Add-Member -MemberType NoteProperty -Name "PoolType" -Value $SourceType
+	$row | Add-Member -MemberType NoteProperty -Name "Protocol" -Value (Get-hvpool $Viewpool.Base.Name).DesktopSettings.DisplayProtocolSettings.DefaultDisplayProtocol
+	$row | Add-Member -MemberType NoteProperty -Name "Available_workstations" -Value ((Get-HVMachineSummary $($viewpool.Base.Name)).Base | Where-Object { $_.BasicState -eq "Available" }).Count
+	$row | Add-Member -MemberType NoteProperty -Name "Connected_workstations" -Value ((Get-HVMachineSummary $($viewpool.Base.Name)).Base | Where-Object { $_.BasicState -eq "Connected" }).Count
+	[array]$tocsv += $row
+}
+
+[array]$tocsv | Export-Csv -Path c:\temp\nonplist.csv -NoTypeInformation
---
+++ Win10_1808_Check_OU.ps1
@@ -0,0 +1,29 @@
+﻿#Windows build below, 1809 = 17763
+$Buildnum = "17763"
+$WinName = "Windows 10"
+
+#Get machine OU 
+$OU = (Get-Adcomputer -identity $env:computername).DistinguishedName -replace "CN=$($env:computername),",""
+
+#Get all machines in previus machine OU
+$Machines = (Get-AdComputer -Filter * -SearchBase $OU).Name
+
+#Check if c$ is accessible - Creating text file with date&time and IP information into machine`s c$
+foreach($Machine in $Machines){
+
+    if(!(Test-Path "\\$($Machine)\c$")){
+    Continue
+    }
+
+    if((Get-WmiObject -ComputerName $Machine -Class Win32_OperatingSystem).Caption -like "*$($WinName)*" -and (get-wmiobject -ComputerName $Machine -Class win32_OperatingSystem).BuildNumber -eq $Buildnum){
+
+    "
+Date & Time = $(Get-Date -Format "dd/MM/yy HH:mm")
+Machine_IP(DNS) = $([System.Net.Dns]::GetHostByName("$($Machine)").AddressList.IpAddressToString)       
+    
+    
+    
+    " | Out-File "\\$($Machine)\c$\$($Machine).txt" -Force
+
+    }
+}
